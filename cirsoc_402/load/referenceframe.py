ec4e7df857907ece9b2792bc28b2114e4e5d5f0a
Pablo <pablombarbieri@gmail.com>
2021-09-09 17:07:14 -0400

    updated reference

--- cirsoc_402/load/referenceframe.py
+++ cirsoc_402/load/referenceframe.py
@@ -1,8 +1,21 @@
-
-
 import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
 import numbers
 import numpy as np
+
+from cirsoc_402.load import Quaternion
+
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, _ = proj3d.proj_transform(xs3d, ys3d, zs3d,self.axes.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer)
 
 
 class ReferenceFrame:
@@ -84,7 +97,7 @@ class ReferenceFrame:
     def zrotate_ref(self, theta):
         self.rotate_along(self.zversor, theta)
     
-    def toorigin(self):
+    def move_to_origin(self):
         self.xshift(-self.xcoord)
         self.yshift(-self.ycoord)
         self.zshift(-self.zcoord)
@@ -107,12 +120,39 @@ class ReferenceFrame:
             theta = np.arccos(np.dot([0, 0, 1], self.zversor))
             self.rotate_along(rotdir, -np.rad2deg(theta))
     
+    def o2r(self, vector):
+        xcomp = np.dot(vector, [np.dot([1, 0, 0], self.xversor), np.dot([0, 1, 0], self.xversor), np.dot([0, 0, 1], self.xversor)])
+        ycomp = np.dot(vector, [np.dot([1, 0, 0], self.yversor), np.dot([0, 1, 0], self.yversor), np.dot([0, 0, 1], self.yversor)])
+        zcomp = np.dot(vector, [np.dot([1, 0, 0], self.zversor), np.dot([0, 1, 0], self.zversor), np.dot([0, 0, 1], self.zversor)])
+        return [xcomp, ycomp, zcomp]
+
+    def r2o(self, vector):
+        xcomp = np.dot(vector, [np.dot([1, 0, 0], self.xversor), np.dot([1, 0, 0], self.yversor), np.dot([1, 0, 0], self.zversor)])
+        ycomp = np.dot(vector, [np.dot([0, 1, 0], self.xversor), np.dot([0, 1, 0], self.yversor), np.dot([0, 1, 0], self.zversor)])
+        zcomp = np.dot(vector, [np.dot([0, 0, 1], self.xversor), np.dot([0, 0, 1], self.yversor), np.dot([0, 0, 1], self.zversor)])
+        return [xcomp, ycomp, zcomp]
+
+    def pos_o2r(self, vector):
+        # position vector relative to the reference frame with its
+        # coordinates expressed in the origin system
+        vector = np.array(vector) - np.array([self.xcoord, self.ycoord, self.zcoord])
+        return self.o2r(vector)
+
+    def pos_r2o(self, vector):
+        # position of the origin relative to the reference frame with
+        # its components expressed the reference frame 
+        origin = self.o2r([-self.xcoord, -self.ycoord, -self.zcoord])
+        # position vector relative to the origin frame with its
+        # coordinates expressed in the reference system
+        vector = vector - np.array(origin)
+        return self.r2o(vector)
+
     def _normalize_versors(self):
         self.xversor =  self.xversor / np.dot(self.xversor, self.xversor)**(1/2)
         self.yversor =  self.yversor / np.dot(self.yversor, self.yversor)**(1/2)
         self.zversor =  self.zversor / np.dot(self.zversor, self.zversor)**(1/2)
         
-    def plot(self, scale=1, margin=0.2, figsize=(8,8)):
+    def plot(self, scale=1, margin=0.2, figsize=(8,8), elev=30, azimut=45):
         fig = plt.figure(figsize=figsize)
         ax = fig.add_subplot(111, projection='3d')
         
@@ -164,11 +204,10 @@ class ReferenceFrame:
         deltax = np.max(xcoords) - np.min(xcoords)
         deltay = np.max(ycoords) - np.min(ycoords)
         deltaz = np.max(zcoords) - np.min(zcoords)
-        delta = np.max([deltax, deltay, deltaz])
         
-        xlims =[np.min(xcoords) - delta * margin, np.min(xcoords) + (1 + margin) * delta]
-        ylims = [np.min(ycoords) - delta * margin, np.min(ycoords) + (1 + margin) * delta]
-        zlims = [np.min(zcoords) - delta * margin, np.min(zcoords) + (1 + margin) * delta]
+        xlims =[np.min(xcoords) - deltax * margin, np.min(xcoords) + (1 + margin) * deltax]
+        ylims = [np.min(ycoords) - deltay * margin, np.min(ycoords) + (1 + margin) * deltay]
+        zlims = [np.min(zcoords) - deltaz * margin, np.min(zcoords) + (1 + margin) * deltaz]
         
         ax.plot([self.ycoord], [self.zcoord], 'ok', zs=xlims[0], zdir='x')
         ax.plot([self.xcoord], [self.zcoord], 'ok', zs=ylims[0], zdir='y')
@@ -196,8 +235,7 @@ class ReferenceFrame:
         ax.set_xlim(xlims)
         ax.set_ylim(ylims)
         ax.set_zlim(zlims)
-
-        
+  
         ax.set_box_aspect([1,1,1])
         ax.set_xlabel('x', fontsize=16)
         ax.set_ylabel('y', fontsize=16)
@@ -207,6 +245,114 @@ class ReferenceFrame:
         ax.set_xticks([0, self.xcoord])
         ax.set_yticks([0, self.ycoord])
         ax.set_zticks([0, self.zcoord])
+        ax.view_init(elev, azimut)
+
+        deltax = xlims[1] - xlims[0]
+        deltay = ylims[1] - ylims[0]
+        deltaz = zlims[1] - zlims[0]
+
+        delta = np.max([deltax, deltay, deltaz])
+        ax.set_box_aspect(aspect = (deltax/delta, deltay/delta, deltaz/delta))
         plt.close(fig)
-        ax.view_init(30, 45)
+        return fig
+
+    def plot_position(self, position, system, scale=1, margin=0.2,
+                      figsize=(8,8), elev=30, azimut=45):
+
+        fig = self.plot(scale=scale, margin=margin, figsize=figsize, 
+                        elev=elev, azimut=azimut)
+        ax = fig.get_axes()[0]
+
+        if not isinstance(system, str):
+            raise TypeError("Reference system must be specified as 'origin' or 'reference'.")
+        if system.lower() == 'origin':
+            poso = position
+            posr = self.pos_o2r(position)
+        elif system.lower() == 'reference':
+            posr = position
+            poso = self.pos_r2o(position)
+        else:
+            raise ValueError("Reference system must be specified as 'origin' or 'reference'.")
+
+        # position arrow from origin
+        arrow = Arrow3D([0, poso[0]],
+                        [0, poso[1]],
+                        [0, poso[2]],
+                        mutation_scale=20, 
+                        lw=3, arrowstyle="-|>", color="g", zorder=2)
+        ax.add_artist(arrow)
+
+        # position arrow from reference system
+        posr_in_o = self.r2o(posr)
+        arrow = Arrow3D([self.xcoord, self.xcoord + posr_in_o[0]],
+                        [self.ycoord, self.ycoord + posr_in_o[1]],
+                        [self.zcoord, self.zcoord + posr_in_o[2]],
+                        mutation_scale=20, 
+                        lw=3, arrowstyle="-|>", color="g", zorder=2)
+        ax.add_artist(arrow)
+
+        # remove gridlines
+        for k in range(12):
+            ax.lines.pop(-1)
+
+        xcoords = [0, scale, self.xcoord, self.xcoord + self.xversor[0] * scale,
+                   self.xcoord + self.yversor[0] * scale,
+                   self.xcoord + self.zversor[0] * scale, poso[0]]
+        ycoords = [0, scale, self.ycoord, self.ycoord + self.xversor[1] * scale,
+                   self.ycoord + self.yversor[1] * scale,
+                   self.ycoord + self.zversor[1] * scale, poso[1]]
+        zcoords = [0, scale, self.zcoord, self.zcoord + self.xversor[2] * scale,
+                   self.zcoord + self.yversor[2] * scale,
+                   self.zcoord + self.zversor[2] * scale, poso[2]]
+        
+        deltax = np.max(xcoords) - np.min(xcoords)
+        deltay = np.max(ycoords) - np.min(ycoords)
+        deltaz = np.max(zcoords) - np.min(zcoords)
+        
+        xlims =[np.min(xcoords) - deltax * margin, np.min(xcoords) + (1 + margin) * deltax]
+        ylims = [np.min(ycoords) - deltay * margin, np.min(ycoords) + (1 + margin) * deltay]
+        zlims = [np.min(zcoords) - deltaz * margin, np.min(zcoords) + (1 + margin) * deltaz]
+        
+        ax.plot([poso[1]], [poso[2]], 'og', zs=xlims[0], zdir='x')
+        ax.plot([poso[0]], [poso[2]], 'og', zs=ylims[0], zdir='y')
+        ax.plot([poso[0]], [poso[1]], 'og', zs=zlims[0], zdir='z')
+        
+        
+        ax.plot([0, 0], zlims, '-k', lw=0.5, alpha=0.4, zs=xlims[0], zdir='x')
+        ax.plot([self.ycoord, self.ycoord], zlims, '-k', lw=0.5, alpha=0.4, zs=xlims[0], zdir='x')
+        ax.plot([poso[1], poso[1]], zlims, '-k', lw=0.5, alpha=0.4, zs=xlims[0], zdir='x')
+        ax.plot(ylims, [0, 0], '-k', lw=0.5, alpha=0.4, zs=xlims[0], zdir='x')
+        ax.plot(ylims, [self.zcoord, self.zcoord], '-k', lw=0.5, alpha=0.4, zs=xlims[0], zdir='x')
+        ax.plot(ylims, [poso[2], poso[2]], '-k', lw=0.5, alpha=0.4, zs=xlims[0], zdir='x')
+
+        ax.plot([0, 0], zlims, '-k', lw=0.5, alpha=0.4, zs=ylims[0], zdir='y')
+        ax.plot([self.xcoord, self.xcoord], zlims, '-k', lw=0.5, alpha=0.4, zs=ylims[0], zdir='y')
+        ax.plot([poso[0], poso[0]], zlims, '-k', lw=0.5, alpha=0.4, zs=ylims[0], zdir='y')
+        ax.plot(xlims, [0, 0], '-k', lw=0.5, alpha=0.4, zs=ylims[0], zdir='y')
+        ax.plot(xlims, [self.zcoord, self.zcoord], '-k', lw=0.5, alpha=0.4, zs=ylims[0], zdir='y')
+        ax.plot(xlims, [poso[2], poso[2]], '-k', lw=0.5, alpha=0.4, zs=ylims[0], zdir='y')
+        
+        ax.plot([0, 0], ylims, '-k', lw=0.5, alpha=0.4, zs=zlims[0], zdir='z')
+        ax.plot([self.xcoord, self.xcoord], ylims, '-k', lw=0.5, alpha=0.4, zs=zlims[0], zdir='z')
+        ax.plot([poso[0], poso[0]], ylims, '-k', lw=0.5, alpha=0.4, zs=zlims[0], zdir='z')
+        ax.plot(xlims, [0, 0],'-k', lw=0.5, alpha=0.4, zs=zlims[0], zdir='z')
+        ax.plot(xlims, [self.ycoord, self.ycoord], '-k', lw=0.5, alpha=0.4, zs=zlims[0], zdir='z')
+        ax.plot(xlims, [poso[1], poso[1]], '-k', lw=0.5, alpha=0.4, zs=zlims[0], zdir='z')
+
+        ax.set_xlim(xlims)
+        ax.set_ylim(ylims)
+        ax.set_zlim(zlims)
+
+        ax.grid(None)
+        ax.set_xticks([0, self.xcoord, poso[0]])
+        ax.set_yticks([0, self.ycoord, poso[1]])
+        ax.set_zticks([0, self.zcoord, poso[2]])
+
+        deltax = xlims[1] - xlims[0]
+        deltay = ylims[1] - ylims[0]
+        deltaz = zlims[1] - zlims[0]
+        delta = np.max([deltax, deltay, deltaz])
+        ax.set_box_aspect(aspect = (deltax/delta, deltay/delta, deltaz/delta))
+
+        plt.close(fig)
         return fig
